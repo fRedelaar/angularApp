4389aa8129bf46fe7bd412a8cdb4bd1f2aad6799
Felicia <fredelaar@gmail.com>
2021-01-21 16:54:36 +0100

    added some features

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,13 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
+  styles: [`
+  .online {
+    color:white;
+  }
+  `]
 })
 export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
   serverName = '';
   serverName3 = 'trying';
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   getServiceStatus() {
     return this.serverStatus;
@@ -18,5 +26,9 @@ export class ServerComponent {
   CreatingServer() {
     this.serverName3 = 'Server was created! Server is named: ' +  this.serverName;
     this.serverName = '';
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
   }
 }
